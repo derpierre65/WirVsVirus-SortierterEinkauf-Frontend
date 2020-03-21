3577381d3c70f6cfed1be060353b2bc2b10e6908
derpierre65 <git@derpierre65.de>
2020-03-21 10:47:33 +0100

    + axios

--- src/client/main.js
+++ src/client/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
 import App from './App.vue';
 import i18n from './i18n';
 import store from './store';
 import router from './router';
+
+Vue.use(VueAxios, axios);
 
 Vue.config.productionTip = false;
 
