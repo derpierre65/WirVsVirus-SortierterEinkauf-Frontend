2ace768271d4c525c8862f29b8021980abe8e5e5
derpierre65 <git@derpierre65.de>
2020-03-21 23:40:06 +0100

    + dark mode

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@
 		<div class="menuBackground">
 			<router-link :to="{name: 'index'}">start</router-link>
 			<router-link :to="{name: 'info'}">info</router-link>
-			<router-link :to="{name: 'market'}">market</router-link>
+			<router-link :to="{name: 'favorite-markets'}">favorite-market</router-link>
+
+			<i class="fa fa32" :class="{'fa-moon-o': !darkMode, 'fa-sun-o': darkMode}" @click="toggleDarkMode"></i>
 		</div>
 		<div class="layoutBoundary">
 			<router-view class="container" />
@@ -12,5 +14,38 @@
 </template>
 
 <script>
-	export default {};
+	export default {
+		data() {
+			let darkMode = false;
+			if (window.localStorage) {
+				darkMode = (window.localStorage.getItem('darkMode') || '1') === '1';
+			}
+
+			return {
+				darkMode
+			};
+		},
+		created() {
+			this.setDarkMode();
+		},
+		methods: {
+			toggleDarkMode() {
+				this.darkMode = !this.darkMode;
+				this.setDarkMode();
+
+				if (window.localStorage) {
+					window.localStorage.setItem('darkMode', this.darkMode ? '1' : '0');
+				}
+			},
+			setDarkMode() {
+				console.log(this.darkMode);
+				if (this.darkMode) {
+					document.body.classList.add('dark');
+				}
+				else {
+					document.body.classList.remove('dark');
+				}
+			}
+		}
+	};
 </script>
